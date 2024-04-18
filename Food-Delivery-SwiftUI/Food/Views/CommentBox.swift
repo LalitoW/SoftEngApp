7fba213fd0c9e2d144641f0b5e62f5802f9046ae
LalitoW <156249322+LalitoW@users.noreply.github.com>
2024-04-18 15:13:09 -0600

    Add files via upload

---
+++ Food-Delivery-SwiftUI/Food/Views/CommentBox.swift
@@ -0,0 +1,18 @@
+//
+//  CommentBox.swift
+//  Food
+//
+//  Created by iOS Lab on 18/04/24.
+//
+
+import SwiftUI
+
+struct CommentBox: View {
+    
+}
+
+struct CommentBox_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentBox()
+    }
+}
