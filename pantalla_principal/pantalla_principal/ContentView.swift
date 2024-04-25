50d62ba6cd8d8aaee87fea571e4fb9301be9e20b
efra333 <75516493+efra333@users.noreply.github.com>
2024-04-25 12:44:50 -0600

    Add files via upload

--- pantalla_principal/pantalla_principal/ContentView.swift
+++ pantalla_principal/pantalla_principal/ContentView.swift
@@ -2,7 +2,6 @@
 //  ContentView.swift
 //  pantalla_principal
 //
-
 import SwiftUI
 
 struct ContentView: View {
@@ -13,47 +12,65 @@ struct ContentView: View {
                 .padding(.top, 20)
                 .padding(.leading, 20)
             
-            RoundedRectangle(cornerRadius: 10)
-                .foregroundColor(Color.orange.opacity(0.5))
-                .frame(height: 100)
-                .padding(.horizontal)
-                .overlay(
-                    HStack {
-                        Spacer()
-                        Image(systemName: "globe")
-                            .foregroundColor(.white)
-                            .frame(width: 50)
-                    }
-                )
-            
-            RoundedRectangle(cornerRadius: 10)
-                .foregroundColor(Color.orange.opacity(0.5))
-                .frame(height: 100)
-                .padding(.horizontal)
-                .overlay(
-                    HStack {
-                        Spacer()
-                        Image(systemName: "globe")
-                            .foregroundColor(.white)
-                            .frame(width: 50)
-                    }
-                )
-            
-            RoundedRectangle(cornerRadius: 10)
-                .foregroundColor(Color.orange.opacity(0.5))
-                .frame(height: 100)
-                .padding(.horizontal)
-                .overlay(
-                    HStack {
-                        Spacer()
-                        Image(systemName: "globe")
-                            .foregroundColor(.white)
-                            .frame(width: 50)
-                    }
-                )
+            RectangleItem(name: "Hamburguesa Americas", price: "$55.00", imageName: "hamburguesa")
+            RectangleItem(name: "Chapata", price: "$65", imageName: "chapata")
+            RectangleItem(name: "Sandwich", price: "$45", imageName: "sandwich")
+            RectangleItem(name: "Chilaquiles", price: "$25", imageName: "chilaquiles")
         }
     }
 }
+
+struct RectangleItem: View {
+    var name: String
+    var price: String
+    var imageName: String
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(Color.orange.opacity(0.5))
+                .frame(height: 150)
+                .padding(.horizontal)
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(name)
+                        .foregroundColor(.black)
+                    
+                    Text(price)
+                        .foregroundColor(.black)
+                    
+
+                    
+                    Button(action: {
+                        // Acción cuando se presiona el botón
+                    }) {
+                        Text("Ordenar")
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 9)
+                            .padding(.vertical, 5)
+                            .background(Color.orange)
+                            .cornerRadius(5)
+                    }
+                }
+                .padding(.leading, 10)
+                
+                Spacer()
+                
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 85)
+                    
+                    .padding(.trailing, 10)
+                    .padding(.top, 25)
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
